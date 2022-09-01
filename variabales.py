7a80412c19563f6299360d8e7fecb55d9c59a0ad
Sadat Hossen Himel <87661192+opeltanvir@users.noreply.github.com>
2022-09-01 22:33:53 +0600

    python

---
+++ variabales.py
@@ -0,0 +1,34 @@
+X = 5
+Y = 'himel'
+
+print(X)
+print(Y)
+
+X = str(3)
+Y = int(3)
+Z = float(3)
+
+T = 5
+R = "john"
+print(type(X))
+print(type(Y))
+
+x,y,z = "orange","Banana" ,"Cherry"
+print(x)
+print(y)
+print(z)
+
+# one value to Multiple variables
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+#Unpack a Collection
+fruits = ["apple","banana","cherry"]
+x,y,z = fruits
+print(x)
+print(y)
+print(z)
+
+
